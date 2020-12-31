88407ed27f47b5f117627fff3175bca39c2cd6ae
Truong Hoang <hqtruong27@gmail.com>
2020-12-31 14:29:30 +0700

    Add IdentityServer (#8)
    * Add IdentityServer
    
    * no change (#7)
    
    * no change
    
    * Add Idenitty Server
    
    * Small change
    
    * Modify ApplicationLayer

--- src/Knowledge.Common/Resources/Constants.cs
+++ src/Knowledge.Common/Resources/Constants.cs
@@ -5,5 +5,9 @@ namespace Knowledge.Common.Resources
     {
         public const string DbContext = "KnowledgeDbContext";
         public const string Environment = "ASPNETCORE_ENVIRONMENT";
+        /// <summary>
+        /// Allow UserName Charactes 
+        /// </summary>
+        public const string UnsignedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
     }
 }
---
+++ src/Knowledge.Web.Portal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Knowledge.Data.EF;
+using Knowledge.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Knowledge.Web.Portal.Areas.Identity.IdentityHostingStartup))]
+namespace Knowledge.Web.Portal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
